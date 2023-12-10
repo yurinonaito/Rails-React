00782ce5e3271c699ff1db3427c09275e48b8da5
Yurino Naito <yurino.naito@gmail.com>
2023-12-10 13:44:55 +0000

    コンポートネントからAPIを叩く練習

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,16 @@
 import React, { Fragment } from 'react';
 
+import { fetchRestaurants } from '../apis/restaurants'; 
+
 export const Restaurants = () => {
+  
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+         )
+    }, [])
+    
   return (
     <Fragment>
       レストラン一覧
