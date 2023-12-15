494c2094435da68779bd0b7da5a9abc575d34c8e
Yurino Naito <yurino.naito@gmail.com>
2023-12-15 14:13:21 +0000

    フード一覧を追加

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,6 +1,18 @@
-import React, { Fragment } from 'react';
 
-export const Foods = () => {
+import React, { Fragment, useEffect } from 'react';
+
+// apis
+import { fetchFoods } from '../apis/foods';
+
+  export const Foods = ({
+    match
+  }) => {
+  useEffect(() => {
+    fetchFoods(1)
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
   return (
     <Fragment>
       フード一覧
